3bf05a9099e8ab45c3707f369a8d06e3f98c05da
lewi01 <isslewi03@gmail.com>
2021-12-22 21:39:09 +0300

    save the content

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.getElementById("lower-case").addEventListener("click", function() {
     document.querySelector('textarea').value.toLowerCase();
 });
 let textarea = document.querySelector('textarea')
+
 document.getElementById("proper-case").addEventListener("click",function(){
     let properSentence = textarea.value.toLowerCase().split(' ');
     let result =[];
@@ -34,3 +35,24 @@ document.getElementById("sentence-case").addEventListener("click",function(){
     textarea.value = result.join('. ')
 
 });
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
+
+document.getElementById("save-text-file").addEventListener("click",function(){
+    let savetext = textarea.value;
+    let file = document.getElementById("text-file").value;
+
+    download(file, savetext);
+},false);
+
+
